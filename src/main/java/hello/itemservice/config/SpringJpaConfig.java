1f70abf4e76292de751b4a5c0b179f4a2c91ddcb
logan <gnlee95@blocko.io>
2024-02-29 02:33:46 +0900

    스프링 데이터 JPA 적용 2

---
+++ src/main/java/hello/itemservice/config/SpringJpaConfig.java
@@ -0,0 +1,27 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepository2;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SpringJpaConfig {
+
+    private final SpringDataJpaItemRepository springDataJpaItemRepository;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepository2(springDataJpaItemRepository);
+    }
+}
